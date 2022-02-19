7a8f60333c1a2b325b38381121ff8a6c586e1638
sreddevil123123 <sreddevil2@gmail.com>
2022-02-19 20:21:13 +0545

    till hero slider

---
+++ JS/script.js
@@ -0,0 +1,157 @@
+"use strict";
+// Hero Slider
+const slides = document.querySelectorAll(".slide");
+const slideBtnleft = document.querySelector(".slider-btn-left");
+const slideBtnRight = document.querySelector(".slider-btn-right");
+const dotsContainer = document.querySelector(".dots");
+const headingHero = document.querySelectorAll(".hero-heading");
+const btnHero = document.querySelectorAll(".btn-primary");
+const textHero = document.querySelectorAll(".hero-info-small");
+
+/////////////////////////////////////////////////////////////
+//HERO SLIDER WITH ANIMATIONS
+/////////////////////////////////////////////////////////////
+let currentSlide = 0;
+const maxSlide = slides.length;
+
+//go to slide with index  and translate
+const goToSlide = function (slideNo) {
+  slides.forEach((s, i) => {
+    s.style.transform = `translateX(${(i - slideNo) * 100}%)`;
+    // to
+    setTimeout(() => {
+      s.style.opacity = 1;
+    }, 1000);
+  });
+};
+
+const createDot = function () {
+  slides.forEach((_, i) => {
+    dotsContainer.insertAdjacentHTML(
+      "beforeend",
+      `<button class="dots__dot" data-slide="${i}"></button>`
+    );
+  });
+};
+
+const activeDotFinder = function (slides) {
+  document
+    .querySelectorAll(".dots__dot")
+    .forEach((dot) => dot.classList.remove("dots__dot--active"));
+  document
+    .querySelector(`.dots__dot[data-slide="${slides}"]`)
+    .classList.add("dots__dot--active");
+};
+
+// Animate the heading
+const animateHeroHeadingBounce = function (slideNo) {
+  headingHero.forEach((heading) =>
+    heading.classList.remove(
+      "animate__animated",
+      "animate__fadeInDown",
+      "animate__delay-1.5s"
+    )
+  );
+  document
+    .querySelector(`.heading-${slideNo + 1}`)
+    .classList.add(
+      "animate__animated",
+      "animate__fadeInDown",
+      "animate__delay-1.5s"
+    );
+};
+// Animate the text
+const animateHeroText = function (slideNo) {
+  textHero.forEach((txt) =>
+    txt.classList.remove(
+      "animate__animated",
+      "animate__shakeX",
+      "animate__delay-1.5s"
+    )
+  );
+  document
+    .querySelector(`.txt-hero-${slideNo + 1}`)
+    .classList.add(
+      "animate__animated",
+      "animate__shakeX",
+      "animate__delay-1.5s"
+    );
+};
+// Animate the Btn
+const animateHeroBtn = function (slideNo) {
+  btnHero.forEach((btn) =>
+    btn.classList.remove(
+      "animate__animated",
+      "animate__slideInUp",
+      "animate__delay-1.5s"
+    )
+  );
+  document
+    .querySelector(`.hero-btn-${slideNo + 1}`)
+    .classList.add(
+      "animate__animated",
+      "animate__slideInUp",
+      "animate__delay-1.5s"
+    );
+};
+
+const initSlider = function () {
+  goToSlide(0);
+  createDot();
+  activeDotFinder(0);
+  animateHeroHeadingBounce(currentSlide);
+  animateHeroText(currentSlide);
+  animateHeroBtn(currentSlide);
+};
+
+const nextSlide = function () {
+  if (currentSlide < maxSlide - 1) {
+    currentSlide++;
+  } else {
+    currentSlide = 0;
+  }
+  goToSlide(currentSlide);
+  activeDotFinder(currentSlide);
+  animateHeroHeadingBounce(currentSlide);
+  animateHeroText(currentSlide);
+  animateHeroBtn(currentSlide);
+};
+
+const prevSlide = function () {
+  if (currentSlide > 0) {
+    currentSlide--;
+  } else {
+    currentSlide = 0;
+  }
+  goToSlide(currentSlide);
+  activeDotFinder(currentSlide);
+  animateHeroHeadingBounce(currentSlide);
+  animateHeroText(currentSlide);
+  animateHeroBtn(currentSlide);
+};
+
+// Event Listeners
+dotsContainer.addEventListener("click", function (e) {
+  if (e.target.classList.contains("dots__dot")) {
+    const currentDotSlide = e.target.dataset.slide;
+    goToSlide(currentDotSlide);
+    activeDotFinder(currentDotSlide);
+    animateHeroHeadingBounce(currentDotSlide);
+    animateHeroText(currentDotSlide);
+    animateHeroBtn(currentDotSlide);
+  }
+});
+
+slideBtnRight.addEventListener("click", nextSlide);
+slideBtnleft.addEventListener("click", prevSlide);
+
+document.addEventListener("keydown", function (e) {
+  console.log(e.key);
+  if (e.key === "ArrowRight") nextSlide();
+  if (e.key === "ArrowLeft") prevSlide();
+});
+initSlider();
+
+/////////////////////////////////////////////////////////////
+//STICKY NAVBAR
+/////////////////////////////////////////////////////////////
